44b9c4639497c5df8eca9b55ff0ac088fd08cfa6
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-10-04 20:39:27 +0530

    Update About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <><Header /><div className='container d-flex flex-column justify-content-center align-items-center vh-100'>
       <h1 className='title text-light'>About us</h1>
       <h3 className='text-light text-center'>
-        Hey there, this is a quiz application where you can check your DSA theoretical skills. I suggest you take 3 tests, and based on the results, you can estimate whether you are good or not. If you get 80% in all the 3 tests, you have good theoretical knowledge of DSA. Practice coding questions, and you will be ready for interviews.
+        Hey there, this is a quiz application where you can check your DSA theoretical skills. After clearing the quix you can start practice coding questions, and you will be ready for interviews.
       </h3>
     </div></>
   );
